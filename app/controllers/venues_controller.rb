7e6cbb7500cf35732f2abd47f483911bcc8e6b4b
Pedro Alonso <pedro@pedroalonso.net>
2016-03-29 18:47:01 +0200

    filter venues by category

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -4,6 +4,7 @@ class VenuesController < ApplicationController
       @venues = []
     else
       @location = address_to_geolocation params[:term]
+      @category = params[:category]
 
       scope = search_by_location
       @total_count = scope.total_count
@@ -18,7 +19,7 @@ class VenuesController < ApplicationController
     end
 
     def search_by_location
-      VenuesIndex
+      scope = VenuesIndex
         .filter {match_all}
         .filter(geo_distance: {
           distance: "2km",
@@ -27,5 +28,11 @@ class VenuesController < ApplicationController
         .order(_geo_distance: {
             location: {lat: @location['lat'], lon: @location['lng']}
           })
+
+      if @category
+        scope = scope.merge(VenuesIndex.filter(match: {categories: @category}))
+      end
+
+      return scope
     end
 end
